302cb1e6b66423eda2d3844abe2bfaef4260a9a9
Klaus <klaus.ostermann@uni-tuebingen.de>
2018-06-25 20:16:05 +0200

    lecture notes

--- lecturenotes/19-shiftreset.scala
+++ lecturenotes/19-shiftreset.scala
@@ -1,3 +1,28 @@
+/**
+Delimited Continuations
+=======================
+The continuations we have seen so far represent the whole call-stack. Invoking a continuation
+was not like a function call because continuations never return a result. Invoking
+a continuation is more similar to a disciplined version of GOTO.
+
+However, the fact that continuations never return and represent the full call-stack often
+makes their use cumbersome. In particular, such continuations cannot be composed. The
+non-composability of continuations is visible in the fact that applications using 
+first-class continuations often need to make use of mutable state.
+
+This is different with _delimited continuations_. Delimited continuations only represent
+a part of the call-stack, namely the part up to the next invocation of ``reset``. Delimited continuations
+behave like functions, that is, they return a value and are hence composable.
+
+Delimited continuations have many quite powerful applications, ranging from advanced exception handling
+to backtracking algorithms and probabilistc programming as well as so-called algebraic effects.
+
+Delimited continuations are available in Racket and many variants of Scheme, OCaML, Haskell and,
+thanks to work in our research group, in Java.
+
+Fortunately, a definitional interpreter for delimited continuations is pretty simple.
+*/
+
 sealed abstract class Exp
 case class Num(n: Int) extends Exp
 case class Id(name: Symbol) extends Exp
@@ -30,9 +55,20 @@ def eval(e: Exp, env: Env, k: Value => Value) : Value = e match {
   
   case App(f,a) => eval(f,env, cl => cl match {
             case ClosureV(f,closureEnv) => eval(a,env, av => eval(f.body, closureEnv + (f.param -> av),k))
-            case ContV(f) => eval(a,env, av => k(f(av)))
+            case ContV(k2) => eval(a,env, av => k(k2(av))) // compose continuations k2 and k
             case _ => sys.error("can only apply functions")
   })
-  case Reset(e) => k(eval(e,env,x=>x))
-  case Shift(param,body) => eval(body, env+(param -> ContV(k)), x=>x) 
+  case Reset(e) => k(eval(e,env,x=>x)) // reset the continuation to the identity function
+  case Shift(param,body) => eval(body, env+(param -> ContV(k)), x=>x)  // wrap current continuation and reset continuation
 }
+
+
+/** 
+References:
+
+Olivier Danvy and Andre Filinski, “Abstracting Control,” LISP and Functional Programming, 1990.
+
+O. Kiselyov, An argument against call/cc. http://okmij.org/ftp/continuations/against-callcc.html
+
+O. Kiselyov, Introduction to programming with shift and reset. http://okmij.org/ftp/continuations/#tutorial
+*/
